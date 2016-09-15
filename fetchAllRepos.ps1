e55559097923fb27d537a52f6264cd35bf2e7fce
Yusuke Hayashi <yhsuzu@gmail.com>
2016-09-15 21:35:20 +0900

    add Double quotation

--- fetchAllRepos.ps1
+++ fetchAllRepos.ps1
@@ -2,7 +2,7 @@ $AllDirectory = Get-ChildItem -Directory -Name
 foreach($dirName in $AllDirectory) {
   cd $dirName
   $location = Get-Location
-  echo $location fetch all
+  echo "$location fetch all"
   git fetch --all
   cd ..
 }
